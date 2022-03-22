96e642125592dd9be705ffca48fab8b90afee108
chenzhangchao <chenzhangchao@cai-inc.com>
2022-03-22 11:28:51 +0800

    feat:个人信息

--- app.go
+++ app.go
@@ -6,10 +6,10 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"name":    "hedui",
-			"age":     23,
-			"message": "Hello World!",
-			"hobby":   "simple is better",
+			"name":     "hedui",
+			"age":      23,
+			"describe": "fron Fu jian sheng",
+			"hobby":    "simple is better",
 		})
 	})
 	r.Run(":8085")
