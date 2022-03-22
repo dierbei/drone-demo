cc2b344d6af7c17db306e22765f8c2780bf892a6
chenzhangchao <chenzhangchao@cai-inc.com>
2022-03-22 08:48:17 +0800

    feat:dev branch

--- app.go
+++ app.go
@@ -6,8 +6,8 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World!",
+			"message": "this is dev branch!",
 		})
 	})
-	r.Run(":8085") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8085")
 }
